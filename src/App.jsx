9785eeb7b65fe1b289d378bc4bd72526258e3012
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-11-27 21:39:05 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+
+function App() {
+
+  return (
+    <div className='bg-red-50'>
+      yyvr67irvi
+    </div>
+  )
+}
+
+export default App
